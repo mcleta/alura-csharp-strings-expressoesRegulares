e2d87bbba77374e2f4e72664d68ef2c073ae80c9
Murillo C Leta <murillocasemiro@gmail.com>
2022-01-21 14:37:49 -0300

    Iniciação do projeto

---
+++ ByteBank.Modelos/ClassTeste.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    class ClassTeste
+    {
+        public ClassTeste()
+        {
+            ModificadoresTeste teste = new ModificadoresTeste();
+        }
+    }
+
+    public class ModificadoresTeste
+    {
+        public void MetodoPublico()
+        {
+            //Visível fora da classe "ModificadoresTeste"
+        }
+        private void MetodoPrivado()
+        {
+            //Visível apenas na classe "ModificadoresTeste"
+        }
+    }
+}
